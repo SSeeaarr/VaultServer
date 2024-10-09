39a59c898ff719bd92ffc6f68c5914fe6d6f7ab5
SSeeaarr <searjaan24@gmail.com>
2024-10-09 00:00:40 -0400

    Add project files.

---
+++ VaultServer/Core.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VaultServer
+{
+    internal class Core
+    {
+        public static async Task Main()
+        {
+
+        
+        
+            try
+            { 
+                int port = 4999;
+                var ip = IPAddress.Parse("10.0.0.6");
+                bool isconnected = false;
+                var cancellationTokenSource = new CancellationTokenSource();
+
+
+                TcpListener server = new TcpListener(ip, port);
+                server.Start();
+
+                string saveDir = @"C:\ServerFiles\";
+
+                if (!Directory.Exists(saveDir))
+                {
+                    Directory.CreateDirectory(saveDir);
+                }
+
+                var connectionstatus = Connectionstatus(cancellationTokenSource.Token, () => isconnected);
+
+                while (true)
+                {
+                    TcpClient stream = await server.AcceptTcpClientAsync();
+                    isconnected = stream.Connected;
+
+                    if (isconnected)
+                    {
+                        cancellationTokenSource.Cancel();
+                        Console.Clear();
+                        Console.WriteLine("Connected to client!");
+                        await Handlefiles.Recieve(saveDir, stream);
+                    }
+                }
+
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Something went wrong: {e.Message} ");
+            }
+        }
+
+        private static async Task Connectionstatus(CancellationToken cancellationToken, Func<bool> isconnected)
+        {
+            char[] chars = { '/', '-', '|' };
+            int i = 0;
+
+            while (!isconnected() && !cancellationToken.IsCancellationRequested)
+            {
+                Console.WriteLine("Waiting for a connection... " + chars[i]);
+                i = (i + 1) % chars.Length;
+                await Task.Delay(500);
+                Console.Clear();
+            }
+
+        }
+
+
+    }
+}
---
+++ VaultServer/Handlefiles.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaultServer
+{
+    internal class Handlefiles
+    {
+        public static async Task Recieve(string path, TcpClient stream)
+        {
+
+            try
+            {
+                using (NetworkStream ns = stream.GetStream())
+                {
+
+                    
+
+                    string filename = "ohio.jpg";
+
+                    using (FileStream fs = new FileStream(path + filename, FileMode.Create))
+                    {
+                        byte[] buffer = new byte[4096];
+                        int readbytes;
+
+                        while ((readbytes = ns.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            fs.Write(buffer, 0, readbytes);
+                        }
+                        
+                        Console.WriteLine("File saved.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occured during the recieve process.");
+            }
+
+        }
+        
+        
+        
+    }
+}
